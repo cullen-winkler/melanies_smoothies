cc6994714fb5db73e2874b720d0058e97bfeaeab
cullen_winkler <122424826+cullen-winkler@users.noreply.github.com>
2024-05-02 01:11:27 -0400

    Update streamlit_app.py
    fixed error.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # Display the Fruit Options List in your Streamlit App.
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 # Convert the Snowpark Dataframe to a pandas dataframe so we can use the LOC function
